aa3253b5240b874587bd92ee0c6954a79d9c5ea3
Zay-074 <zay074@qq.com>
2026-03-14 21:59:22 +0800

    feat:add update status with redis

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -31,11 +31,12 @@ public class DishServiceImpl implements DishService {
     private DishMapper dishMapper;
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
-
     @Autowired
     private SetMealDishMapper setMealDishMapper;
+
     /**
      * 新增菜品和对应的口味
+     *
      * @param dishDTO
      */
     @Override
@@ -43,13 +44,13 @@ public class DishServiceImpl implements DishService {
     public void saveWithFlavor(DishDTO dishDTO) {
         //向菜品表插入1条数据
         Dish dish = new Dish();
-        BeanUtils.copyProperties(dishDTO,dish);
+        BeanUtils.copyProperties(dishDTO, dish);
         dishMapper.insert(dish);
         //获取插入后的id
         Long dishId = dish.getId();
         //向口味表插入n条数据
         List<DishFlavor> flavors = dishDTO.getFlavors();
-        if(flavors != null && !flavors.isEmpty()){
+        if (flavors != null && !flavors.isEmpty()) {
 
             for (DishFlavor flavor : flavors) {
                 flavor.setDishId(dishId); //设置菜品id
@@ -66,14 +67,15 @@ public class DishServiceImpl implements DishService {
      */
     @Override
     public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
-        PageHelper.startPage(dishPageQueryDTO.getPage(),dishPageQueryDTO.getPageSize());    //开启分页 startPage(当前页码，每页数据量)
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());    //开启分页 startPage(当前页码，每页数据量)
         Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
 
-        return new PageResult(page.getTotal(),page);
+        return new PageResult(page.getTotal(), page);
     }
 
-    /**
+    /** 
      * 批量删除菜品
+     *
      * @param ids
      */
     @Transactional
@@ -82,14 +84,14 @@ public class DishServiceImpl implements DishService {
         //判断当前菜品是否能够删除，查看菜品status是否为1
         for (Long id : ids) {
             Dish dish = dishMapper.getById(id);
-            if(dish.getStatus() == StatusConstant.ENABLE){
+            if (dish.getStatus() == StatusConstant.ENABLE) {
                 throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
             }
         }
 
         //判断菜品是否关联了套餐
         List<Long> setMealIds = setMealDishMapper.getSetMealIdsByDishIds(ids);
-        if (setMealIds != null && !setMealIds.isEmpty()){
+        if (setMealIds != null && !setMealIds.isEmpty()) {
             //当前菜品关联了套餐
             throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
         }
@@ -107,6 +109,7 @@ public class DishServiceImpl implements DishService {
 
     /**
      * 根据id查询菜品和对应的口味数据
+     *
      * @param id
      * @return
      */
@@ -118,19 +121,21 @@ public class DishServiceImpl implements DishService {
         List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(id);
         //封装DishVO
         DishVO dishVO = new DishVO();
-        BeanUtils.copyProperties(dish,dishVO);
+        BeanUtils.copyProperties(dish, dishVO);
         dishVO.setFlavors(dishFlavors);
         return dishVO;
     }
+
     /**
      * 修改菜品
+     *
      * @param dishDTO
      */
     @Override
     public void updateWithFlavor(DishDTO dishDTO) {
         //修改dish表
         Dish dish = new Dish();
-        BeanUtils.copyProperties(dishDTO,dish);
+        BeanUtils.copyProperties(dishDTO, dish);
         dishMapper.update(dish);
 
         //修改dish_flavor表
@@ -138,7 +143,7 @@ public class DishServiceImpl implements DishService {
         dishFlavorMapper.deleteByDishId(dishDTO.getId());
         //插入当前菜品对应的口味数据
         List<DishFlavor> flavors = dishDTO.getFlavors();
-        if(flavors != null && !flavors.isEmpty()){
+        if (flavors != null && !flavors.isEmpty()) {
             flavors.forEach(dishFlavor -> dishFlavor.setDishId(dishDTO.getId()));
             dishFlavorMapper.insertBatch(flavors);
         }
